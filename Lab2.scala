c7fefcd1a421046ec24b4821a565bb03fefbeec4
Lucas Sward <lusw6126@colorado.edu>
2018-02-06 18:19:49 -0700

    Updated more cases
    Check these operations for more failing tests, "<",<=,>,>=. Specifically with characters

--- Lab2.scala
+++ Lab2.scala
@@ -9,7 +9,7 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
   /*
    * CSCI 3155: Lab 2
    * <Lucas Sward>
-   * 
+   *
    * Partner: <Your Partner's Name>
    * Collaborators: <Any Collaborators>
    */
@@ -17,16 +17,16 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
   /*
    * Fill in the appropriate portions above by replacing things delimited
    * by '<'... '>'.
-   * 
+   *
    * Replace the '???' expression with  your code in each function.
-   * 
+   *
    * Do not make other modifications to this template, such as
    * - adding "extends App" or "extends Application" to your Lab object,
    * - adding a "main" method, and
    * - leaving any failing asserts.
-   * 
+   *
    * Your lab will not be graded if it does not compile.
-   * 
+   *
    * This template compiles without error. Before you submit comment out any
    * code that does not compile or causes a failing assert. Simply put in a
    * '???' as needed to get something  that compiles without error. The '???'
@@ -36,7 +36,7 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
 
   /* We represent a variable environment as a map from a string of the
    * variable name to the value to which it is bound.
-   * 
+   *
    * You may use the following provided helper functions to manipulate
    * environments, which are just thin wrappers around the Map type
    * in the Scala standard library.  You can use the Scala standard
@@ -61,13 +61,14 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
   def toNumber(v: Expr): Double = {
     require(isValue(v))
     (v: @unchecked) match {
+      case Undefined => Double.NaN
       case N(n) => n
       case B(false) => 0
       case B(true) => 1
       case S("") => 0
       case S("Undefined") => Double.NaN
       case S("undefined") => Double.NaN
-      case S(str) => {try str.toDouble catch {case _: Throwable => Double.NaN}}
+      case S(str) => {try str.toDouble catch {case e: java.lang.NumberFormatException => Double.NaN}}
       case _ => ???
     }
   }
@@ -99,7 +100,15 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
   def eval(env: Env, e: Expr): Expr = {
     e match {
       /* Base Cases */
-      case Var(x) => lookup(env, x)
+      case Var(x) => try {
+        lookup(env, x)
+      }
+    catch{
+    case e: java.util.NoSuchElementException => {
+      val variable = x
+      eval(env, Print(S(s"ReferenceError: $variable is not defined")))
+    }
+    }
       case N(_)|S(_)|B(_)|Undefined => e
 
       /* Inductive Cases */
@@ -113,42 +122,87 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
         if (toBoolean(v1) == true) return eval(env, e2) else eval(env, e1)
       }
       case Binary(Or, e1, e2) => {
-        val v1 = eval(e1)
-        if (toBoolean(v1) == true) return e1 else eval(e2)
+        val v1 = eval(env, e1)
+        if (toBoolean(v1) == true) return e1 else eval(env, e2)
+      }
+      // We are doing "===" so make some test cases for that
+
+        // case for two strings
+      case Binary(Eq, S(s), S(t)) => eval(env, B(toStr(S(s)) == toStr(S(t))))
+        // case for two undefined expressions
+      case Binary(Eq, Undefined, Undefined) => B(true)
+        // case for undefined and a string
+      case Binary(Eq, Undefined, S(s)) => B(false)
+        // case for undefined and a number
+      case Binary(Eq, Undefined, N(n)) => B(false)
+        // case for two numbers
+      case Binary(Eq, N(n), N(m)) => eval(env, B(toNumber(N(n)) == toNumber(N(m))))
+        // case for a number and undefined
+      case Binary(Eq, N(n), Undefined) => B(false)
+        // case for a string and undefined
+      case Binary(Eq, S(s), Undefined) => B(false)
+      // case for two booleans
+      case Binary(Eq, B(b), B(a)) => eval(env, B(toBoolean(B(b)) == toBoolean(B(a))))
+        // cases for boolean and undefined
+      case Binary(Eq, B(b), Undefined) => B(false)
+      case Binary(Eq, Undefined, B(b)) => B(false)
+        // cases for differing types
+      case Binary(Eq, S(s), N(n)) => B(false)
+      case Binary(Eq, N(n), S(s)) => B(false)
+      case Binary(Eq, B(b), N(n)) => B(false)
+      case Binary(Eq, N(n), B(b)) => B(false)
+      case Binary(Eq, S(s), B(b)) => B(false)
+      case Binary(Eq, B(b), S(s)) => B(false)
+
+
+      case Binary(Eq, e1, e2) => eval(env, Binary(Eq, eval(env, e1), eval(env, e2)))
+
+
+
+      case Binary(Plus, e1, S(str)) => eval(env, S(toStr(eval(env, e1)) + toStr(eval(env,S(str)))))
+      case Binary(Plus, S(str), e2) => eval(env, S(toStr(S(str)) + toStr(eval(env, e2))))
+      case Binary(Plus, N(n), N(n2)) => eval(env, N(toNumber(N(n)) + toNumber(N(n2))))
+      case Binary(Plus, Undefined, N(n)) => N(Double.NaN)
+      case Binary(Plus, N(n), Undefined) => N(Double.NaN)
+      case Binary(Plus, B(b), B(b2)) => eval(env, N(toNumber(B(b)) + toNumber(B(b2))))
+      case Binary(Plus, B(b), N(n)) => eval(env, N(toNumber(B(b)) + toNumber(N(n))))
+      case Binary(Plus, N(n), B(b)) => eval(env, N(toNumber(B(b)) + toNumber(N(n))))
+      case Binary(Plus, Undefined, e2) => N(Double.NaN)
+      case Binary(Plus, e1, Undefined) => N(Double.NaN)
+      case Binary(Plus, e1, e2) => {
+        eval(env, Binary(Plus,eval(env, e1),eval(env, e2)))
       }
 
-      case Binary(Eq, e1, e2) => eval(B(toNumber(eval(e1)) == toNumber(eval(e2))))
 
 
-      case Binary(Plus, e1, e2) => ???
 
-      case Binary(Ge, e1, e2) => eval(B(toNumber(eval(e1)) >= toNumber(eval(e2))))
+      case Binary(Ge, e1, e2) => eval(env, B(toNumber(eval(env, e1)) >= toNumber(eval(env, e2))))
 
-      case Binary(Gt, e1, e2) => eval(B(toNumber(eval(e1)) > toNumber(eval(e2))))
+      case Binary(Gt, e1, e2) => eval(env, B(toNumber(eval(env, e1)) > toNumber(eval(env, e2))))
 
-      case Binary(Le, e1, e2) => eval(B(toNumber(eval(e1)) <= toNumber(eval(e2))))
+      case Binary(Le, e1, e2) => eval(env, B(toNumber(eval(env, e1)) <= toNumber(eval(env, e2))))
 
 
-      case Binary(Minus, e1, e2) => eval(N(toNumber(eval(e1))-toNumber(eval(e2))))
+      case Binary(Minus, e1, e2) => eval(env, N(toNumber(eval(env, e1))-toNumber(eval(env, e2))))
 
-      case Binary(Times, e1, e2) => eval(N(toNumber(eval(e1)) * toNumber(eval(e2))))
+      case Binary(Times, e1, e2) => eval(env, N(toNumber(eval(env, e1)) * toNumber(eval(env, e2))))
 
-      case Binary(Div, e1, e2) => eval(N(toNumber(eval(e1)) / toNumber(eval(e2))))
+      case Binary(Div, e1, e2) => eval(env, N(toNumber(eval(env, e1)) / toNumber(eval(env, e2))))
 
-      case Binary(Ne, e1, e2) => eval(B(toNumber(eval(e1)) != toNumber(eval(e2))))
+      case Binary(Ne, e1, e2) => eval(env, B(toNumber(eval(env, e1)) != toNumber(eval(env, e2))))
 
-      case Binary(Lt, e1, e2) => eval(B(toNumber(eval(e1)) < toNumber(eval(e2))))
+      case Binary(Lt, e1, e2) => eval(env, B(toNumber(eval(env, e1)) < toNumber(eval(env, e2))))
 
-      case Unary(Neg, e1) => eval(N(-toNumber(eval(e1))))
+      case Unary(Neg, e1) => eval(env, N(-toNumber(eval(env, e1))))
 
-      case Unary(Not, e1) => eval(B(!toBoolean(eval(e1))))
+      case Unary(Not, e1) => eval(env, B(!toBoolean(eval(env, e1))))
 
       case If(e1, e2, e3) => {
-        val b1 = eval(e1)
-        if(toBoolean(b1) == true) eval(e2) else eval(e3)
+        val b1 = eval(env, e1)
+        if(toBoolean(b1) == true) eval(env, e2) else eval(env, e3)
       }
 
-      case Binary(Seq, e1, e2) => eval(e1);eval(e2)
+      case Binary(Seq, e1, e2) => eval(env, e1);eval(env, e2)
 
 
       case Print(e1) => println(pretty(eval(env, e1))); Undefined
@@ -174,7 +228,7 @@ object Lab2 extends jsy.util.JsyApplication with Lab2Like {
 
     val v = eval(expr)
 
-     println(pretty(v))
+    println(pretty(v))
   }
 
 }
